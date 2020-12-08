9e9bc7ead7054ba786f8145af7edbc05b11f4a0d
ClepceaBeatrice <48672842+ClepceaBeatrice@users.noreply.github.com>
2020-12-08 17:12:12 +0200

    Update main.cpp
    Am adaugat o noua metoda doSomething(const string n)  prin care am aratat functionalitatea unui reference-counting smart pointer.  La iesirea din functie, pInv1 si pInv2 sunt distruse, iar obiectul catre care pointeaza este sters automat.  Am folosit una dintre cele mai folosite clase RAII, tr1::shared_ptr, pentru a preveni scurgerile de resurse.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING  1
+
 #include <iostream>
 #include "dreptunghi.h"
 #include "paralelipiped.h"
+#include "cub.h"
+#include "figGeo.h"
+
 
 using namespace std;
 
+
+figGeo *createFig() {
+    return new figGeo("nume");
+}
+
+void doSomething(const string n) {
+    tr1::shared_ptr<figGeo>plnv1(figGeo::construct(n));
+    tr1::shared_ptr<figGeo>plnv2(plnv1);
+
+    plnv1 = plnv2;
+
+    cout << plnv1->getNume() << endl;
+    cout << plnv2->getNume() << endl;
+}
+
+
 int main() {
-    dreptunghi d1("d1",4, 5), d2("d1",10, 2);
+    dreptunghi d1("d1", 4, 5), d2("d1", 10, 2);
     dreptunghi d3 = d1; //copy constructor is called here
     d2 = d1;            //assigment operator is called here
     cout << d2.getNume() << " " << d2.getL() << "\t" << d1.getNume() << " " << d1.getL() << endl;
@@ -14,15 +35,31 @@ int main() {
     cout << d3.Arie() << endl;
     cout << "Diagonala d1 este: " << diagonala(d1) << endl;
 
-
     paralelipiped p1(d1, d2, d3);
     cout << "Volumul paralelipipedului: " << p1.Volum() << endl;
+
+
+
+    dreptunghi d4("d4", 8, 9), d5("d5", 9, 11), d6("d6", 12, 18);
+    d4 = d5 = d6; //demonstrate multiple assigment
+    cout << "d4 are L: " << d4.getL() << "\t" << " d5 are L: " << d5.getL() << "\t" << "d6 are L:" << d6.getL() << endl;
+
+    d4 += d1;
+    cout << "Latimea d4: " << d4.getl() << endl;
+
+    d6 = 3;
+    cout << "Latimea d6: " << d6.getl() << endl;
+
+    cub c1(5);
+    c1.afisare();
+    c1 = c1;
+    c1.afisare();
+    cub c2(10);
+    c2.afisare();
+    c1 = c2;
+    c1.afisare();
     
-    dreptunghi d4("d4",8,9),d5("d5",9,11),d6("d6",12,18);
-    d4=d5=d6; //demonstrate multiple assigment
-    cout<<"d4 are L: " <<d4.getL()<< "\t" <<" d5 are L: "<<d5.getL()<< "\t" <<"d6 are L:"<<d6.getL()<<endl;
-    d1 += d4;
-    cout<<"d1 este: "<<d1.getl()<<endl;
-    //paralelipiped p2 = p1;  eroare de compilare
+    doSomething("Shared Pointer");
+
     return 0;
 }
